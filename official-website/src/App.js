382fcd0138ffbc0c9ea1fd8a163b3e7718a2205c
Jeffrey Zhu <1624410543@qq.com>
2025-03-14 23:06:55 +0800

    初始化企业官网

--- official-website/src/App.js
+++ official-website/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+ * @Author: Jeffrey Zhu 1624410543@qq.com
+ * @Date: 2025-03-14 22:27:37
+ * @LastEditors: Jeffrey Zhu 1624410543@qq.com
+ * @LastEditTime: 2025-03-14 22:58:19
+ * @FilePath: \Smart-Snap-AI\official-website\src\App.js
+ * @Description: File Description Here...
+ * 
+ * Copyright (c) 2025 by JeffreyZhu, All Rights Reserved. 
+ */
+import React from 'react'
+import Header from './widgets/Header'
+import Hero from './widgets/Hero'
+import Solutions from './widgets/Solutions'
+import HowItWorks from './widgets/HowItWorks'
+import Clients from './widgets/Clients'
+import Contact from './widgets/Contact'
+import Footer from './widgets/Footer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Index() {
+    return (
+        <div className='container mx-auto p-4'>
+            <Header />
+            <main className='mt-20'>
+                <Hero />
+                <section className='md:p-20'>
+                    <Solutions />
+                </section>
+                <section className='mt-20'>
+                    <HowItWorks />
+                </section>
+
+                <section className='mt-20 md:w-3/5 mx-auto'>
+                    <Clients />
+                </section>
+
+                <section className='mt-20'>
+                    <Contact />
+                </section>
+
+                <section className='mt-20'>
+                    <Footer />
+                </section>
+
+            </main>
+        </div>
+    )
 }
 
-export default App;
+export default Index
---
+++ official-website/tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  // 指定项目目录下那些文件需要处理！ 
+   content: [
+     "./index.html",
+     "./src/**/*.{js,ts,jsx,tsx}",
+   ],
+   theme: {
+     extend: {},
+   },
+   plugins: [],
+ }
+ 
